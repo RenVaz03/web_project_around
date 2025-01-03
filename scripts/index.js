6c5ad034fc6cc2bb1d4e663d4f2941b923d2c3bc
renyvazval <renyvazval@gmail.com>
2025-01-02 19:25:45 -0600

    cambios echos a las clases BEM

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const openModal = document.querySelector('.profile__info_edit');
+const openModal = document.querySelector('.profile__info-edit');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.modal__close');
 const saveButton = document.querySelector('.modal__save');
