7e428fbbd3d6f6456bb556696323326063679056
renyvazval <renyvazval@gmail.com>
2025-02-06 11:12:14 -0600

    se crea funcion para crear mas tarjetas

--- scripts/index.js
+++ scripts/index.js
@@ -7,16 +7,6 @@ const aboutInput = document.querySelector('.modal__abt-me');
 const profileName = document.querySelector('.profile__info-name');
 const profileProfession = document.querySelector('.profile__info-profession');
 
-const openAdd = document.querySelector('.profile__button-add')
-const add = document.querySelector('.add');
-const closeAdd = document.querySelector('.add__close');
-const addSaveButton = document.querySelector('.add__save');
-const titleInput = document.querySelector('.add__title');
-const urlInput = document.querySelector('.add__url');
-const cardTitle = document.querySelector('.card__title-name');
-const cardLink = document.querySelector('.card__link');
-const cardGallery = document.querySelector('.gallery')
-const likeButton = document.querySelector('.gallery__button-like')
 
 openModal.addEventListener('click', (e) => {
     e.preventDefault();
@@ -44,6 +34,15 @@ saveButton.addEventListener('click', (e) => {
     modal.classList.remove('modal--active');
 });
 
+const openAdd = document.querySelector('.profile__button-add')
+const add = document.querySelector('.add');
+const closeAdd = document.querySelector('.add__close');
+const addSaveButton = document.querySelector('.add__save');
+const titleInput = document.querySelector('.add__title');
+const urlInput = document.querySelector('.add__url');
+const cardTitle = document.querySelector('.card__title-name');
+const likeButton = document.querySelector('.gallery__button-like')
+
 openAdd.addEventListener('click', (e) => {
     e.preventDefault();
     add.classList.add('add--active');
@@ -60,3 +59,48 @@ e.preventDefault();
 add.classList.remove('add--active');
 });
 
+
+const photoGallery = document.querySelector('.photo__gallery');
+
+
+function createCard(title, imageUrl) {
+    const card = document.createElement('div');
+    card.classList.add('gallery');
+
+ 
+    const cardImage = document.createElement('img');
+    cardImage.src = imageUrl;
+    cardImage.alt = title;
+    cardImage.classList.add('gallery__images');
+
+
+    const cardDetails = document.createElement('div');
+    cardDetails.classList.add('gallery__images-element');
+
+    const cardTitle = document.createElement('p');
+    cardTitle.textContent = title;
+    cardTitle.classList.add('gallery__image-name');
+
+    const likeButton = document.createElement('button');
+    likeButton.classList.add('gallery__button-like');
+    const likeIcon = document.createElement('img');
+    likeIcon.src = './images/HeartButton.png';
+    likeIcon.alt = 'boton de like';
+    likeButton.appendChild(likeIcon);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('gallery__button-delete');
+    const deleteIcon = document.createElement('img');
+    deleteIcon.src = './images/trashButton.png';
+    deleteIcon.alt = 'boton de basura';
+    deleteButton.appendChild(deleteIcon);
+
+    
+    cardDetails.appendChild(cardTitle);
+    cardDetails.appendChild(likeButton);
+    cardDetails.appendChild(deleteButton);
+    card.appendChild(cardImage);
+    card.appendChild(cardDetails);
+
+    return card;
+}
