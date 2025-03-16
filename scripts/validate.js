3173804c59e72b62652cc35e9c896cc8161bdb4b
renyvazval <renyvazval@gmail.com>
2025-03-15 19:03:05 -0600

    se agrega codigo para mensajes de validacion

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,64 +1,60 @@
- const enableValidation = () => {
-    const forms = document.querySelectorAll("form");
-  
-    forms.forEach((form) => {
-      const inputs = form.querySelectorAll("input");
-      const submitButton = form.querySelector("button[type='submit']");
-  
-      const showError = (input) => {
-        const errorElement = form.querySelector(`.${input.id}__error`);
-        if (errorElement) {
-          errorElement.textContent = input.validationMessage;
-          errorElement.classList.add("modal__error-active");
-          input.classList.add("modal__input-error");
-        }
-      };
-  
-      const hideError = (input) => {
-        const errorElement = form.querySelector(`.${input.id}__error`);
-        if (errorElement) {
-          errorElement.textContent = "";
-          errorElement.classList.remove("modal__error-active");
-          input.classList.remove("modal__input-error");
-        }
-      };
-  
-      const checkInputValidity = (input) => {
-        if (!input.validity.valid) {
-          showError(input);
-        } else {
-          hideError(input);
-        }
-      };
-  
-      const checkFormValidity = () => {
-        if (form.checkValidity()) {
-          submitButton.disabled = false;
-        } else {
-          submitButton.disabled = true;
-        }
-      };
-  
-      inputs.forEach((input) => {
-        input.addEventListener("input", () => {
-          checkInputValidity(input);
-          checkFormValidity();
-        });
+const enableValidation = () => {
+  const forms = document.querySelectorAll("form");
+
+  forms.forEach((form) => {
+    const inputs = form.querySelectorAll("input");
+    const submitButton = form.querySelector("button[type='submit']");
+
+    const showError = (input) => {
+      const errorElement = form.querySelector(`.${input.id}__error`);
+      if (errorElement) {
+        errorElement.textContent = input.validationMessage;
+        errorElement.classList.add("modal__error-active");
+        input.classList.add("modal__input-error");
+      }
+    };
+
+    const hideError = (input) => {
+      const errorElement = form.querySelector(`.${input.id}__error`);
+      if (errorElement) {
+        errorElement.textContent = "";
+        errorElement.classList.remove("modal__error-active");
+        input.classList.remove("modal__input-error");
+      }
+    };
+
+    const checkInputValidity = (input) => {
+      if (!input.validity.valid) {
+        showError(input);
+      } else {
+        hideError(input);
+      }
+    };
+
+    const checkFormValidity = () => {
+      submitButton.disabled = !form.checkValidity();
+    };
+
+    inputs.forEach((input) => {
+      input.addEventListener("input", () => {
+        input.reportValidity();
+        checkInputValidity(input);
+        checkFormValidity();
       });
-  
-      form.addEventListener("submit", function (evt) {
-        if (!form.checkValidity()) {
-
-        } else {
-
-          evt.preventDefault();
-          form.reset();
-          checkFormValidity();
-        }
-      });
-
-      checkFormValidity();
     });
-  };
-  
-  enableValidation();
+
+    form.addEventListener("submit", function (evt) {
+      if (!form.checkValidity()) {
+        evt.preventDefault();
+        form.reportValidity();
+      } else {
+        form.reset();
+        checkFormValidity();
+      }
+    });
+
+    checkFormValidity();
+  });
+};
+
+enableValidation();
