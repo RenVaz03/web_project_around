cab322ed8fd5d31e1211132150fef4595a420b1e
renyvazval <renyvazval@gmail.com>
2025-04-08 18:24:49 -0600

    se agrega codigo y clase Card a Card.js

---
+++ scripts/card.js
@@ -0,0 +1,51 @@
+export class Card {
+    constructor(data, templateSelector) {
+        this._title = data.name
+        this._image = data.link
+        this._templateSelector =templateSelector;
+    }
+    _getTemplate(){
+        const template = document
+        .querySelector(this._templateSelector)
+        .content
+        .querySelector(".gallery")
+        .cloneNode(true);
+
+    return template;
+    }
+
+    generateCard() {
+        this._element = this._getTemplate();
+        this._cardImage = this._element.querySelector(".gallery__images");
+        this._cardTitle = this._element.querySelector('.gallery__image-name');
+        this._likeButton = this._element.querySelector('.gallery__button-like');
+        this._deleteButton = this._element.querySelector('.gallery__button-delete');
+    
+        this._cardImage.src = this._image;
+        this._cardImage.alt = this._title;
+        this._cardTitle.textContent = this._title;
+    
+        this._setEventListeners();
+    
+        return this._element;
+    }
+
+    _setEventListeners(){
+        this._likeButton.addEventListener("click", () => this._handleLike());
+        this._deleteButton.addEventListener("click", () => this._handleDelete());
+        this._cardImage.addEventListener("click", () => this._handlePreview());
+    }
+
+    _handleLike(){
+        this._likeButton.classList.toggle("gallery__button-like_active");
+    }
+
+    _handleDelete(){
+        this._element.remove();
+        this._element = null;
+    }
+
+    _handlePreview(){
+        openImagePopup(this._image, this._title);
+    }
+}
